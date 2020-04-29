d7c020e70c63c77112a1710f85f8d9ba05c9b8e6
Ankur Verma (SF) <ankurv@corp.microsoft.com>
2020-04-28 20:16:36 -0700

    Vcpkg fixes

--- ci/build_with_vcpkg.sh
+++ ci/build_with_vcpkg.sh
@@ -1,8 +1,6 @@
-scriptdir = $(dirname $(realpath $0))
-
+scriptdir=$(dirname $(realpath $0))
 export VCPKG_USE_STENCIL_SRC_DIR=${scriptdir}/..
-
 git clone https://github.com/Microsoft/vcpkg.git
-git -C vcpkg apply --ignore-space-change --ignore-whitespace $scriptdir/vcpkg/*patch
+git -C vcpkg apply --ignore-space-change --ignore-whitespace $scriptdir/vcpkg/vcpkg.stencil.patch
 vcpkg/bootstrap-vcpkg.sh
 vcpkg/vcpkg install stencil
