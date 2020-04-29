927557e6261c45e247531c3e38d3c382096fb3af
Ankur Verma (SF) <ankurv@corp.microsoft.com>
2020-04-28 17:05:16 -0700

    Pipeline fixes

--- ci/build_with_vcpkg.bat
+++ ci/build_with_vcpkg.bat
@@ -1,5 +1,5 @@
 git clone https://github.com/Microsoft/vcpkg.git
 set VCPKG_USE_STENCIL_SRC_DIR="%~dp0\.."
-git apply --ignore-space-change --ignore-whitespace $scriptdir/vcpkg/*patch
+git apply --ignore-space-change --ignore-whitespace %~dp0/vcpkg/*patch
 vcpkg/bootstrap-vcpkg.bat
-vcpkg/vcpkg install stencil:%1
+vcpkg/vcpkg install stencil:x86-windows
