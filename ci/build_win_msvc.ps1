6cc7beaaf08afe19834c25b50761312c5fd1b124
Ankur Verma <ankur.verma@outlook.com>
2020-05-23 17:45:53 -0700

    Pipeline fixes

--- ci/build_win_msvc.ps1
+++ ci/build_win_msvc.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 $vs2019_path = (@()+(&"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -property installationpath))[-1]
 Import-Module (Join-Path $vs2019_path "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
 Enter-VsDevShell -VsInstallPath $vs2019_path -SkipAutomaticLocation -DevCmdArguments "-host_arch=amd64 -arch=amd64"
-echo "Root $PSScriptRoot"
+
 cmake.exe $PSScriptRoot\..
 cmake.exe --build . -j --target package
 ctest.exe -C Debug
