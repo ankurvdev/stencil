b89db39bcce3898d4fafdf2571d4702842fe12de
Ankur Verma <ankur.verma@outlook.com>
2021-02-19 01:47:37 -0800

    Use python

--- ci/build_with_vcpkg.py
+++ ci/build_with_vcpkg.py
@@ -5,14 +5,17 @@ import shutil
 import subprocess
 import sys
 
-parser = argparse.ArgumentParser(description="Test VCPKG WOrkflow")
+parser = argparse.ArgumentParser(description="Test VCPKG Workflow")
 parser.add_argument("--workdir", type=str, default=".", help="Root")
 parser.add_argument("--triplet", type=str, default=None, help="Triplet")
 args = parser.parse_args()
-if not os.path.exists("vcpkg"):
-    subprocess.check_call("git clone -q https://github.com/ankurverma85/vcpkg.git --branch ankurv/stencil --depth 1", cwd=args.workdir)
 
+os.makedirs(args.workdir, exist_ok=True)
 vcpkgroot = os.path.join(args.workdir, "vcpkg")
+
+if not os.path.exists(vcpkgroot):
+    subprocess.check_call(["git", "clone", "-q", "https://github.com/ankurverma85/vcpkg.git", "--branch", "ankurv/stencil", "--depth 1"], cwd=args.workdir)
+
 stencilportdir = os.path.join(vcpkgroot, "ports", "stencil")
 scriptdir = os.path.abspath(os.path.dirname(__file__))
 bootstrapscript = "bootstrap-vcpkg.bat" if sys.platform == "win32" else "bootstrap-vcpkg.sh"
@@ -38,8 +41,8 @@ def TestVcpkgBuild(config):
         "-DVCPKG_TARGET_TRIPLET=" + triplet,
         "-DVCPKG_VERBOSE:BOOL=ON", 
         os.path.join(scriptdir, "vcpkg")], cwd=testdir)
-    subprocess.check_call("cmake --build . -j --target package" + (("--config " + config) if sys.platform == "win32" else ""), cwd=testdir)
-    subprocess.check_call("ctest . " + (("-C " + config) if sys.platform == "win32" else ""), cwd=testdir)
+    subprocess.check_call(["cmake", "--build", ".", "-j", "--target", "package", + (["--config", config] if sys.platform == "win32" else [])], cwd=testdir)
+    subprocess.check_call(["ctest", ".", + (["-C", config] if sys.platform == "win32" else [])], cwd=testdir)
 
 TestVcpkgBuild("Debug")
 TestVcpkgBuild("Release")
