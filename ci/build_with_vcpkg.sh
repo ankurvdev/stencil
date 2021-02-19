c93bd04d05094b18be3e4430585e6e29381b7364
Ankur Verma <ankur.verma@outlook.com>
2021-02-19 00:35:56 -0800

    Fix linux builds

--- ci/build_with_vcpkg.sh
+++ ci/build_with_vcpkg.sh
@@ -1,3 +1,6 @@
+set -e
+set -o pipefail
+
 scriptdir=$(dirname $(realpath $0))
 git clone -q https://github.com/ankurverma85/vcpkg.git --branch ankurv/stencil --depth 1
 rm -rf vcpkg/ports/stencil
@@ -5,13 +8,15 @@ cp -r ${scriptdir}/vcpkg/port.stencil vcpkg/ports/stencil
 echo ${scriptdir}/.. > vcpkg/ports/stencil/use_source_path
 vcpkg/bootstrap-vcpkg.sh
 vcpkg/vcpkg install stencil
+
 mkdir Test-Debug
 pushd Test-Debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DVCPKG_ROOT=$(realpath ./vcpkg) -DVCPKG_VERBOSE:BOOL=ON $scriptdir\vcpkg
+cmake -DCMAKE_BUILD_TYPE=Debug -DVCPKG_ROOT=$(realpath ./vcpkg) -DVCPKG_VERBOSE:BOOL=ON $scriptdir/vcpkg
 cmake --build . -j --target package
 ctest .
+
 mkdir Test-Release
 pushd Test-Release
-cmake -DCMAKE_BUILD_TYPE=Release -DVCPKG_ROOT=$(realpath ./vcpkg) -DVCPKG_VERBOSE:BOOL=ON $scriptdir\vcpkg
+cmake -DCMAKE_BUILD_TYPE=Release -DVCPKG_ROOT=$(realpath ./vcpkg) -DVCPKG_VERBOSE:BOOL=ON $scriptdir/vcpkg
 cmake --build . -j --target package
 ctest .
