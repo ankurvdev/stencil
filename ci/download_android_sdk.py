11f42ac2b5eba49b09923d26587abac1f5fda64b
Ankur Verma <ankur.verma@outlook.com>
2021-08-22 17:25:35 -0700

    update download sdk script

--- ci/download_android_sdk.py
+++ ci/download_android_sdk.py
@@ -17,6 +17,7 @@ import html.parser
 import time
 import threading
 
+
 class HTMLUrlExtractor(html.parser.HTMLParser):
     def __init__(self, url):
         text = urllib.request.urlopen(url, timeout=10, context=ssl._create_unverified_context()).read().decode("utf-8")
@@ -43,6 +44,7 @@ class HTMLUrlExtractor(html.parser.HTMLParser):
         if self.href is not None:
             self.text = data
 
+
 def SearchFilename(dir, name):
     for path, dirs, fls in os.walk(dir):
         if name in fls:
@@ -52,9 +54,9 @@ def SearchFilename(dir, name):
 
 def SearchExecutable(bindir, binname):
     if sys.platform == "win32":
-        path = SearchFilename(bindir, binname + ".exe") or SearchFilename(bindir, binname + ".bat") or SearchFilename(bindir, binname + ".cmd")
+        path = SearchFilename(bindir, binname + ".bat") or SearchFilename(bindir, binname + ".cmd") or SearchFilename(bindir, binname + ".exe")
     elif sys.platform == "linux":
-        path = SearchFilename(bindir, binname)
+        path = SearchFilename(bindir, binname + ".sh") or SearchFilename(bindir, binname)
         if path:
             os.chmod(path, os.stat(path).st_mode | stat.S_IEXEC)
     return path
@@ -78,40 +80,64 @@ def GetBinary(binname, bindir):
         return exe
     return None
 
+
 def AcceptSDKLicenses(path):
     proc = subprocess.Popen([GetBinary('sdkmanager', path), f"--sdk_root={path}", '--licenses'], stdin=subprocess.PIPE)
     while proc.poll() is None:
         time.sleep(1)
         proc.communicate(input=b'y\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\n')
 
+
 def AddToPath(dir):
     dir = os.path.abspath(dir)
     if dir not in os.environ['PATH'].split(os.pathsep):
         os.environ['PATH'] = os.pathsep.join([dir, os.environ['PATH']])
 
-def DownloadSdkManager(path):
-    urls = HTMLUrlExtractor("https://developer.android.com/studio").urls
-    ossuffix = {"linux" : "linux", "win32" : "win"}[sys.platform]
-    pattern =  f"https://dl.google.com/android/repository/commandlinetools-{ossuffix}.*.zip"
-    url = [u for u in urls.keys() if re.match(pattern, u)][0]
 
+def DownloadSdkManager(path):
+    sdkpath = os.path.join(path, 'sdk')
+    os.makedirs(sdkpath, exist_ok=True)
+    urls = HTMLUrlExtractor("https://developer.android.com/studio").urls
+    ossuffix = {"linux": "linux", "win32": "win"}[sys.platform]
+    pattern = f"https://dl.google.com/android/repository/commandlinetools-{ossuffix}.*.zip"
+    url = [u for u in urls.keys() if re.match(pattern, u)][0]
     downloadtofile = os.path.join(path, "downloads", "commandlinetools.zip")
+    if not os.path.exists(downloadtofile):
+        print(f"Download {url} to {downloadtofile}")
+        urllib.request.urlretrieve(url, downloadtofile)
+    shutil.unpack_archive(downloadtofile, sdkpath)
+    AcceptSDKLicenses(path)
+
+def DownloadAndroidStudio(path):
+    studiopath = path
+    ossuffix = {"linux": "linux", "win32": "win"}[sys.platform]
+    ext = {"linux": "tar.gz", "win32": "zip"}[sys.platform]
+    urls = HTMLUrlExtractor("https://developer.android.com/studio").urls
+    ossuffix = {"linux": "linux", "win32": "windows"}[sys.platform]
+    pattern = f"https://redirector.gvt1.com/edgedl/android/studio/.*/android-studio-.*-{ossuffix}.{ext}"
+    url = [u for u in urls.keys() if re.match(pattern, u)][0]
+    downloadtofile = os.path.join(path, "downloads", f"studio.{ext}")
     os.makedirs(os.path.dirname(downloadtofile), exist_ok=True)
     if not os.path.exists(downloadtofile):
         print(f"Download {url} to {downloadtofile}")
         urllib.request.urlretrieve(url, downloadtofile)
     shutil.unpack_archive(downloadtofile, path)
-    AcceptSDKLicenses(path)
 
 def GetAndroidSdkManager(path):
     return GetBinary('sdkmanager', path) or DownloadSdkManager(path) or GetBinary('sdkmanager', path)
 
+def GetAndroidStudio(path):
+    return GetBinary('studio', path) or DownloadAndroidStudio(path) or GetBinary('studio', path)
+
 def DownloadJava(path):
+    return DownloadAndroidStudio(path) # Just use java from android studio
     urls = HTMLUrlExtractor("https://docs.microsoft.com/en-us/java/openjdk/download").urls
-    ossuffix = {"linux" : "linux", "win32" : "windows"}[sys.platform]
-    pattern =  f"https://aka.ms/download-jdk/microsoft-jdk-16.*{ossuffix}-x64.*"
+    ossuffix = {"linux": "linux", "win32": "windows"}[sys.platform]
+    pattern = f"https://aka.ms/download-jdk/microsoft-jdk-16.*{ossuffix}-x64.*"
     url = [u for u in urls.keys() if re.match(pattern, u)][0]
-    ext = {"linux" : "tar.gz", "win32" : "zip"}[sys.platform]
+    #if sys.platform == "linux":
+    #    url = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=244058_89d678f2be164786b292527658ca1605'
+    ext = {"linux": "tar.gz", "win32": "zip"}[sys.platform]
     downloadtofile = os.path.join(path, "downloads", f"jdk.{ext}")
     os.makedirs(os.path.dirname(downloadtofile), exist_ok=True)
     if not os.path.exists(downloadtofile):
@@ -119,16 +145,35 @@ def DownloadJava(path):
         urllib.request.urlretrieve(url, downloadtofile)
     shutil.unpack_archive(downloadtofile, path)
 
+
 def GetJava(path):
     return GetBinary('java', path) or DownloadJava(path) or GetBinary('java', path)
 
-def DownloadTo(path:str):
+
+def DownloadTo(path: str):
     java = GetJava(path)
     sdkmanager = GetAndroidSdkManager(path)
-    os.environ["PATH"] = os.pathsep.join([os.path.dirname(java) , os.path.dirname(sdkmanager), os.environ["PATH"]])
-    AcceptSDKLicenses(path)
-    subprocess.check_call([sdkmanager, f"--sdk_root={path}", "ndk-bundle", "ndk;22.1.7171670", "build-tools;31.0.0",  "platform-tools", "platforms;android-31"])
-    return {"ndk": os.path.abspath(os.path.join(path, "ndk-bundle"))}
+    sdkpath = os.path.join(path, 'sdk')
+    os.environ["PATH"] = os.pathsep.join([os.path.dirname(java), os.path.dirname(sdkmanager), os.environ["PATH"]])
+    dirs = {''}
+    for d in os.scandir(sdkpath):
+        dirs.add(d.name)
+    packages = ["ndk-bundle", "ndk;23.0.7599858", "build-tools;31.0.0", "platform-tools", "platforms;android-31"]
+    def IsNeeded():
+        for p in packages:
+            if not p.split(';')[0] in dirs: return True
+        return False
+    retval = {
+        "ndk": os.path.abspath(os.path.join(path, "ndk-bundle")),
+        "java_home": os.path.dirname(os.path.dirname(java)),
+        "sdk_root": sdkpath
+    }
+    if not IsNeeded(): return retval
+    cmd = [sdkmanager, f"--sdk_root={sdkpath}"] + packages
+    print(" ".join(cmd))
+    subprocess.check_call(cmd)
+    return retval
+
 
 if __name__ == "__main__":
     DownloadTo(sys.argv[1])
