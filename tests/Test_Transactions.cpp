fe0b1a6a8cd8db6528708c72cee3db891b5a6a12
ankurv <ankurv@microsoft.com>
2021-10-20 13:03:27 -0700

    Fixes

--- tests/Test_Transactions.cpp
+++ tests/Test_Transactions.cpp
@@ -21,18 +21,18 @@ TEST_CASE("Transactions", "[Transactions]")
     TestReplay replay;
     replay.Replay("imu.pitchDeg1E6 = 1000000;imu.pitchDeg1E6 = 1000000;gps.loc.lat1E7 = 10000000;gps.loc.lon1E7 = 10000000");
     replay.Replay("imu.pitchDeg1E6 = 2000000;imu.pitchDeg1E6 = 2000000;gps.loc.lat1E7 = 20000000;gps.loc.lon1E7 = 20000000");
-    replay.Replay("traffic.aircrafts:add[0] = {addr: 100}");
-    replay.Replay("traffic.aircrafts:add[0] = {addr: 200}");
-    replay.Replay("traffic.aircrafts:add[0] = {addr: 300}");
-    replay.Replay("traffic.aircrafts:add[0] = {addr: 400}");
-    replay.Replay("traffic.aircrafts:add[0] = {addr: 500}");
+    replay.Replay("traffic.aircrafts:add[0] = {\"addr\": 100}");
+    replay.Replay("traffic.aircrafts:add[0] = {\"addr\": 200}");
+    replay.Replay("traffic.aircrafts:add[0] = {\"addr\": 300}");
+    replay.Replay("traffic.aircrafts:add[0] = {\"addr\": 400}");
+    replay.Replay("traffic.aircrafts:add[0] = {\"addr\": 500}");
     replay.Replay("traffic.aircrafts.0.loc.lat1E7 = 30000001;");
     replay.Replay("traffic.aircrafts.1.loc.lat1E7 = 30000002;");
     replay.Replay("traffic.aircrafts.2.loc.lat1E7 = 30000003;");
     replay.Replay("traffic.aircrafts.3.loc.lat1E7 = 30000004;");
     replay.Replay("traffic.aircrafts.4.loc.lat1E7 = 30000005;");
-    replay.Replay("traffic.aircrafts:remove[2]");
-    replay.Replay("traffic.aircrafts:remove[2]");
+    replay.Replay("traffic.aircrafts:remove[2] = {}");
+    replay.Replay("traffic.aircrafts:remove[2] = {}");
     replay.Replay("environment.pressureHgIn1E7 = 110000000");
     replay.Replay("settings.altimeterHgIn1E7 = 222000000");
 
