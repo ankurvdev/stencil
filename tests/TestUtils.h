944d15bc41bb8a4c17e6ef875d7a45636de68dbb
Ankur Verma <ankur.verma@outlook.com>
2021-10-31 15:42:36 -0700

    Using comming test utils

--- tests/TestUtils.h
+++ tests/TestUtils.h
@@ -4,11 +4,11 @@
 #pragma warning(push, 3)
 #pragma warning(disable : 4868)
 #pragma warning(disable : 4738)
-#pragma warning(disable : 4365)    // signed unsigned mismatch
-#pragma warning(disable : 5219)    // implicit conversion from 'uint64_t' to 'double', possible loss of data
-#pragma warning(disable : 5204)    // class has virtual functions, but its trivial destructor is not virtual
-#pragma warning(disable : 4668)    // not defined as a preprocessor macro
-#pragma warning(disable : 5039)    // pointer or reference to potentially throwing function passed to 'extern)
+#pragma warning(disable : 4365) // signed unsigned mismatch
+#pragma warning(disable : 5219) // implicit conversion from 'uint64_t' to 'double', possible loss of data
+#pragma warning(disable : 5204) // class has virtual functions, but its trivial destructor is not virtual
+#pragma warning(disable : 4668) // not defined as a preprocessor macro
+#pragma warning(disable : 5039) // pointer or reference to potentially throwing function passed to 'extern)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Weverything"
 
@@ -23,6 +23,7 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include <span>
 DECLARE_RESOURCE_COLLECTION(testdata);
 
 inline std::string wstring_to_string(std::wstring_view wstr)
@@ -35,15 +36,18 @@ inline std::string wstring_to_string(std::wstring_view wstr)
     return out;
 }
 
-inline std::vector<std::string> readlines(std::filesystem::path const& filepath)
+inline std::vector<std::string> readlines(std::filesystem::path const &filepath)
 {
     std::vector<std::string> lines;
-    std::ifstream            file(filepath);
-    std::string              line;
+    std::ifstream file(filepath);
+    std::string line;
 
     while (std::getline(file, line))
     {
-        if (line.length() > 0 && line[line.length() - 1] == '\r') { line.resize(line.length() - 1); }
+        if (line.length() > 0 && line[line.length() - 1] == '\r')
+        {
+            line.resize(line.length() - 1);
+        }
         lines.push_back(std::move(line));
     }
     return lines;
@@ -54,49 +58,63 @@ struct ResourceFileManager
     ResourceFileManager() = default;
     ~ResourceFileManager()
     {
-        for (auto const& [k, v] : _openedfiles) { std::filesystem::remove(v); }
+        for (auto const &[k, v] : _openedfiles)
+        {
+            std::filesystem::remove(v);
+        }
     }
 
-    auto load(std::string const& name, std::string const& prefix)
+    auto load(std::string const &name, std::string const &prefix)
     {
-        auto it = _openedfiles.find(name);
-        if (it != _openedfiles.end()) { return it->second; }
+        auto testresname = Catch::getResultCapture().getCurrentTestName() + name;
+
+        auto it = _openedfiles.find(testresname);
+        if (it != _openedfiles.end())
+        {
+            return it->second;
+        }
         auto resourceCollection = LOAD_RESOURCE_COLLECTION(testdata);
         for (auto const r : resourceCollection)
         {
-            if (wstring_to_string(r.name()) == name)
+            if (wstring_to_string(r.name()) == testresname)
             {
-                auto          path = std::filesystem::current_path() / (prefix + name);
+                auto path = std::filesystem::current_path() / (prefix + testresname);
                 std::ofstream f(path);
-                auto const&   str = r.string();
-                if (!f.is_open()) { throw std::runtime_error("Cannot write resource file : " + path.string()); }
+                auto const &str = r.string();
+                if (!f.is_open())
+                {
+                    throw std::runtime_error("Cannot write resource file : " + path.string());
+                }
                 f << str;
                 f.close();
-                _openedfiles[name] = path;
+                _openedfiles[testresname] = path;
                 return path;
             }
         }
-        throw std::logic_error("Cannot find resource : " + name);
+        throw std::logic_error("Cannot find resource : " + testresname);
     }
 
     std::unordered_map<std::string, std::filesystem::path> _openedfiles;
 };
 
-inline void CompareLines(std::vector<std::string> const& actualstring,
-                         std::vector<std::string> const& expectedstring,
-                         std::string const&              resname = "test")
+inline void CompareLines(std::vector<std::string> const &actualstring,
+                         std::vector<std::string> const &expectedstring,
+                         std::string_view const &resname = "test")
 {
 
     dtl::Diff<std::string, std::vector<std::string>> d(expectedstring, actualstring);
-    d.compose();                // construct an edit distance and LCS and SES
-    d.composeUnifiedHunks();    // construct a difference as Unified Format with SES.
+    d.compose();             // construct an edit distance and LCS and SES
+    d.composeUnifiedHunks(); // construct a difference as Unified Format with SES.
 
     if (actualstring != expectedstring)
     {
-        d.printUnifiedFormat();    // print a difference as Unified Format.
+        d.printUnifiedFormat(); // print a difference as Unified Format.
         {
-            std::ofstream f(resname + "_failure.txt");
-            for (auto& l : actualstring) { f << l << std::endl; }
+            std::ofstream f(std::string(resname) + "_failure.txt");
+            for (auto &l : actualstring)
+            {
+                f << l << std::endl;
+            }
             f.flush();
             f.close();
         }
@@ -104,15 +122,15 @@ inline void CompareLines(std::vector<std::string> const& actualstring,
     }
 }
 
-inline void CheckOutputAgainstResource(std::vector<std::string> const& lines, std::string const& resourcename)
+inline void CheckOutputAgainstResource(std::vector<std::string> const &lines, std::string_view const &resourcename)
 {
     ResourceFileManager resfiles;
-    CompareLines(lines, readlines(resfiles.load(resourcename, "res_")), resourcename);
+    CompareLines(lines, readlines(resfiles.load(std::string(resourcename), "res_")), resourcename);
 }
 
 inline void CompareBinaryOutputAgainstResource(std::span<const uint8_t> const &actual, std::string_view const &resourcename)
 {
-    auto testresname = Catch::getResultCapture().getCurrentTestName() + std::string(resourcename);
+    auto testresname = Catch::getResultCapture().getCurrentTestName() + "_" + std::string(resourcename);
     auto resourceCollection = LOAD_RESOURCE_COLLECTION(testdata);
     for (auto const r : resourceCollection)
     {
@@ -124,7 +142,7 @@ inline void CompareBinaryOutputAgainstResource(std::span<const uint8_t> const &a
             return;
         }
     }
-    FAIL("Cannot find reference resource");
+    FAIL("Cannot find reference resource", testresname);
 }
 inline void CompareFileAgainstResource(std::filesystem::path const &actualf, std::string_view const &resourcename)
 {
