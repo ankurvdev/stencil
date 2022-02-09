06b38416d16ad2a5b36140710a1860aeb90dcca7
ankurv <ankurv@microsoft.com>
2022-02-08 20:01:49 -0800

    Json test cases complete

--- tests/HandwrittenRuntime/Test_Handwritten_Json.cpp
+++ tests/HandwrittenRuntime/Test_Handwritten_Json.cpp
@@ -120,18 +120,18 @@ TEST_CASE("Json", "[Json]")
             "Nested");
     }
 
-#if 0
     SECTION("MultiAttributed")
     {
-        RunTestCases<MultiAttributed>({
-            {R"({"f1": {"timestamp": "2020-01-02:03:04:05.600"}})", "multi-attributed-1", true},
-            {R"({"f1": {"timestamp": "bad-timestamp"}})", "multi-attributed-2", false},
-            {R"({"f1": {"uuid": "{01234567-8901-2345-6789-012345678901}"}})", "multi-attributed-3", true},
-            {R"({"f1": {"uuid": "01234567-8901-2345-6789-012345678901"}})", "multi-attributed-3", true},
-            {R"({"f1": {"uuid": 0}})", "multi-attributed-3", false},
-            {R"({"f1": {"f1", {}}})", "multi-attributed-3", true},
-            {R"({"f1": {"timestamp": "2020-01-02:03:04:05.600", "uuid": "01234567-8901-2345-6789-012345678901", "f1", {}}})", "", true},
-        });
+        RunTestCases<MultiAttributed>(
+            {
+                {R"({"f1": {"timestamp": "2020-01-02:03:04:05.600"}})", "multi-attributed-1", true},
+                {R"({"f1": {"timestamp": "bad-timestamp"}})", "multi-attributed-2", false},
+                {R"({"f1": {"uuid": "{01234567-8901-2345-6789-012345678901}"}})", "multi-attributed-3", true},
+                {R"({"f1": {"uuid": "01234567-8901-2345-6789-012345678901"}})", "multi-attributed-3", true},
+                {R"({"f1": {"uuid": 0}})", "multi-attributed-3", false},
+                {R"({"f1": {"f1", {}}})", "multi-attributed-3", true},
+                {R"({"f1": {"timestamp": "2020-01-02:03:04:05.600", "uuid": "01234567-8901-2345-6789-012345678901", "f1", {}}})", "", true},
+            },
+            "MultiAttributed");
     }
-#endif
 }
