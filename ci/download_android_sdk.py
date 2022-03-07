514ad2a93328c88e98eb69c53d3e8eb0d4b7a84a
Ankur Verma <ankur.verma@outlook.com>
2022-03-07 00:45:17 -0800

    Merge Files

--- ci/download_android_sdk.py
+++ ci/download_android_sdk.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+# pylint: disable=invalid-name
+
+import html.parser
 import os
-from posixpath import dirname
-import sys
+import pathlib
+import re
+import shutil
+import ssl
 import stat
 import subprocess
 import sys
-import urllib.request
-import urllib.parse
-import ssl
-import pathlib
-import zipfile
-import shutil
-import platform
-import re
-import html.parser
 import time
-import threading
+import urllib.parse
+import urllib.request
+from typing import Callable, Optional
 
 
 class HTMLUrlExtractor(html.parser.HTMLParser):
@@ -44,66 +42,65 @@ class HTMLUrlExtractor(html.parser.HTMLParser):
         if self.href is not None:
             self.text = data
 
+    def error(self, _message: str) -> None:
+        pass
 
-def SearchFilename(dir, name):
-    for path, dirs, fls in os.walk(dir):
+
+def _search_filename(path: pathlib.Path, name) -> Optional[pathlib.Path]:
+    for fpath, _dirs, fls in os.walk(path):
         if name in fls:
-            return os.path.join(path, name)
+            return pathlib.Path(fpath) / name
     return None
 
 
-def SearchExecutable(bindir, binname):
+def _search_exe(bindir: pathlib.Path, binname) -> Optional[pathlib.Path]:
     if sys.platform == "win32":
-        path = SearchFilename(bindir, binname + ".bat") or SearchFilename(bindir, binname + ".cmd") or SearchFilename(bindir, binname + ".exe")
+        path = _search_filename(bindir, binname + ".bat") or _search_filename(bindir,
+                                                                              binname + ".cmd") or _search_filename(bindir, binname + ".exe")
     elif sys.platform == "linux":
-        path = SearchFilename(bindir, binname + ".sh") or SearchFilename(bindir, binname)
+        path = _search_filename(bindir, binname + ".sh") or _search_filename(bindir, binname)
         if path:
             os.chmod(path, os.stat(path).st_mode | stat.S_IEXEC)
     return path
 
 
-def MoveUp(dir):
-    for p in os.scandir(dir):
-        shutil.move(p.path, os.path.join(dir, ".."))
-    os.rmdir(dir)
+def _move_up(path: pathlib.Path):
+    for p in os.scandir(path):
+        shutil.move(p.path, path.parent)
+    os.rmdir(path)
 
 
-def GetBinary(binname, bindir):
-    exe = None # shutil.which(binname)
+def _download_or_get_Binary(binname, bindir: pathlib.Path, downloadFn: Callable[[pathlib.Path], None]) -> pathlib.Path:
+    exe = None  # shutil.which(binname)
     if exe is not None:
-        if sys.platform == "win32" and os.path.basename(os.path.dirname(exe)) != "system32":
+        if sys.platform == "win32" and exe.parent.name != "system32":
             return exe
     os.makedirs(bindir, exist_ok=True)
-    exe = SearchExecutable(bindir, binname)
+    exe = _search_exe(bindir, binname)
     if exe is not None:
-        AddToPath(os.path.dirname(exe))
+        AddToPath(exe.parent)
         return exe
-    return None
+    downloadFn(bindir)
+    exe = _search_exe(bindir, binname)
+    if exe is not None:
+        AddToPath(exe.parent)
+        return exe
+    raise Exception(f"Cannot find or download {binname} in {bindir}")
 
 
-def AcceptSDKLicenses(path):
-    sdkpath = os.path.join(path, 'sdk')
-    cmd = [GetBinary('sdkmanager', path), f"--sdk_root={sdkpath}", '--licenses']
-    sys.stderr.write(" ".join(cmd) + "\n")
-    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
-    while proc.poll() is None:
-        time.sleep(1)
-        proc.communicate(input=b'y\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\n')
+def AddToPath(path: pathlib.Path):
+    path = path.absolute()
+    if path not in os.environ['PATH'].split(os.pathsep):
+        os.environ['PATH'] = os.pathsep.join([str(path), os.environ['PATH']])
 
 
-def AddToPath(dir):
-    dir = os.path.abspath(dir)
-    if dir not in os.environ['PATH'].split(os.pathsep):
-        os.environ['PATH'] = os.pathsep.join([dir, os.environ['PATH']])
-
-
-def DownloadSdkManager(path):
+def DownloadSdkManager(path: pathlib.Path):
     sdkpath = os.path.join(path, 'sdk')
     os.makedirs(sdkpath, exist_ok=True)
     urls = HTMLUrlExtractor("https://developer.android.com/studio").urls
     ossuffix = {"linux": "linux", "win32": "win"}[sys.platform]
     pattern = f"https://dl.google.com/android/repository/commandlinetools-{ossuffix}.*.zip"
-    url = [u for u in urls.keys() if re.match(pattern, u)][0]
+    url = [u for u in urls if re.match(pattern, u)][0]
     downloadtofile = os.path.join(path, "downloads", "commandlinetools.zip")
     if not os.path.exists(downloadtofile):
         print(f"Download {url} to {downloadtofile}")
@@ -111,14 +108,14 @@ def DownloadSdkManager(path):
     shutil.unpack_archive(downloadtofile, sdkpath)
     AcceptSDKLicenses(path)
 
-def DownloadAndroidStudio(path):
-    studiopath = path
+
+def DownloadAndroidStudio(path: pathlib.Path):
     ossuffix = {"linux": "linux", "win32": "win"}[sys.platform]
     ext = {"linux": "tar.gz", "win32": "zip"}[sys.platform]
     urls = HTMLUrlExtractor("https://developer.android.com/studio").urls
     ossuffix = {"linux": "linux", "win32": "windows"}[sys.platform]
     pattern = f"https://redirector.gvt1.com/edgedl/android/studio/.*/android-studio-.*-{ossuffix}.{ext}"
-    url = [u for u in urls.keys() if re.match(pattern, u)][0]
+    url = [u for u in urls if re.match(pattern, u)][0]
     downloadtofile = os.path.join(path, "downloads", f"studio.{ext}")
     os.makedirs(os.path.dirname(downloadtofile), exist_ok=True)
     if not os.path.exists(downloadtofile):
@@ -126,55 +123,60 @@ def DownloadAndroidStudio(path):
         urllib.request.urlretrieve(url, downloadtofile)
     shutil.unpack_archive(downloadtofile, path)
 
-def GetAndroidSdkManager(path):
-    return GetBinary('sdkmanager', path) or DownloadSdkManager(path) or GetBinary('sdkmanager', path)
 
-def GetAndroidStudio(path):
-    return GetBinary('studio', path) or DownloadAndroidStudio(path) or GetBinary('studio', path)
-
-def DownloadJava(path):
-    return DownloadAndroidStudio(path) # Just use java from android studio
-    urls = HTMLUrlExtractor("https://docs.microsoft.com/en-us/java/openjdk/download").urls
-    ossuffix = {"linux": "linux", "win32": "windows"}[sys.platform]
-    pattern = f"https://aka.ms/download-jdk/microsoft-jdk-16.*{ossuffix}-x64.*"
-    url = [u for u in urls.keys() if re.match(pattern, u)][0]
-    #if sys.platform == "linux":
-    #    url = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=244058_89d678f2be164786b292527658ca1605'
-    ext = {"linux": "tar.gz", "win32": "zip"}[sys.platform]
-    downloadtofile = os.path.join(path, "downloads", f"jdk.{ext}")
-    os.makedirs(os.path.dirname(downloadtofile), exist_ok=True)
-    if not os.path.exists(downloadtofile):
-        print(f"Download {url} to {downloadtofile}")
-        urllib.request.urlretrieve(url, downloadtofile)
-    shutil.unpack_archive(downloadtofile, path)
+def AcceptSDKLicenses(path: pathlib.Path):
+    sdkpath = os.path.join(path, 'sdk')
+    cmd = [str(_download_or_get_Binary('sdkmanager', path, DownloadSdkManager)), f"--sdk_root={sdkpath}", '--licenses']
+    sys.stderr.write(" ".join(cmd) + "\n")
+    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    while proc.poll() is None:
+        time.sleep(1)
+        proc.communicate(input=b'y\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\ny\n')
 
 
-def GetJava(path):
-    return GetBinary('java', path) or DownloadJava(path) or GetBinary('java', path)
+def GetAndroidSdkManager(path: pathlib.Path) -> pathlib.Path:
+    return _download_or_get_Binary('sdkmanager', path, DownloadSdkManager)
+
+
+def GetAndroidStudio(path: pathlib.Path) -> pathlib.Path:
+    return _download_or_get_Binary('studio', path, DownloadAndroidStudio)
+
+
+def DownloadJava(path: pathlib.Path):
+    return DownloadAndroidStudio(path)  # Just use java from android studio
+
+
+def GetJava(path: pathlib.Path) -> pathlib.Path:
+    return _download_or_get_Binary('java', path, DownloadJava)
+
 
 def GetJarSigner(path):
-    return GetBinary('jarsigner', path) or DownloadJava(path) or GetBinary('jarsigner', path)
+    return _download_or_get_Binary('jarsigner', path, DownloadJava)
 
-def DownloadTo(path: str):
+
+def DownloadTo(path: pathlib.Path):
     java = GetJava(path)
     sdkmanager = GetAndroidSdkManager(path)
-    sdkpath = os.path.join(path, 'sdk')
-    os.environ["PATH"] = os.pathsep.join([os.path.dirname(java), os.path.dirname(sdkmanager), os.environ["PATH"]])
+    sdkpath = path / 'sdk'
+    os.environ["PATH"] = os.pathsep.join([str(java.parent), str(sdkmanager.parent), os.environ["PATH"]])
     dirs = {''}
     for d in os.scandir(sdkpath):
         dirs.add(d.name)
     packages = ["ndk-bundle", "ndk;23.0.7599858", "build-tools;31.0.0", "platform-tools", "platforms;android-31"]
+
     def IsNeeded():
         for p in packages:
-            if not p.split(';')[0] in dirs: return True
+            if not p.split(';', maxsplit=1)[0] in dirs:
+                return True
         return False
     retval = {
-        "ndk": os.path.abspath(os.path.join(sdkpath, "ndk-bundle")),
-        "java_home": os.path.dirname(os.path.dirname(java)),
+        "ndk": (sdkpath / "ndk-bundle").absolute(),
+        "java_home": java.parent.parent,
         "sdk_root": sdkpath
     }
-    if not IsNeeded(): return retval
-    cmd = [sdkmanager, f"--sdk_root={sdkpath}"] + packages
+    if not IsNeeded():
+        return retval
+    cmd = [str(sdkmanager), f"--sdk_root={sdkpath}"] + packages
     sys.stderr.write(" ".join(cmd) + "\n")
     subprocess.check_call(cmd)
     AcceptSDKLicenses(path)
@@ -182,4 +184,4 @@ def DownloadTo(path: str):
 
 
 if __name__ == "__main__":
-    DownloadTo(sys.argv[1])
+    DownloadTo(pathlib.Path(sys.argv[1]))
