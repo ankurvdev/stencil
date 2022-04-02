ef3e5c5d027e839cfd306b2f3111721c8206c1dd
ankurv <ankurv@microsoft.com>
2022-04-02 00:22:38 -0700

    Fix

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -3,6 +3,6 @@ set -e
 set -o pipefail
 set -x
 
-cmake -DCMAKE_CXX_FLAGS=-stdlib=libstdc++-11 $(dirname $(realpath $0))/..
+cmake $(dirname $(realpath $0))/..
 cmake --build . -j --target package
 ctest
