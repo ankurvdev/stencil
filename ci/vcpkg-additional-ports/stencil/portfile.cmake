940eb82e7c560123e9d5aa92cb5670dd087918fb
Ankur Verma <31362771+ankurvdev@users.noreply.github.com>
2022-06-22 22:26:56 -0700

    Add CI pipeline job for auto-updating vcpkg port (#48)

--- ci/vcpkg-additional-ports/stencil/portfile.cmake
+++ ci/vcpkg-additional-ports/stencil/portfile.cmake
@@ -13,8 +13,8 @@ elseif (DEFINED ENV{VCPKG_USE_STENCIL_GIT_REPO} AND NOT "$ENV{VCPKG_USE_STENCIL_
 else()
     # sha512sum can be obtained using
     # wget -O - -q  https://github.com/ankurverma85/stencil/archive/b6429f8b92947273a5e66d5f10210b960616a89d.tar.gz | sha512sum 
-    set(commitId b6429f8b92947273a5e66d5f10210b960616a89d)
-    set(sha512 3e306ded5c984a9a00db1d06a02873fcdc104f9c92ce577821b4caed2907f928e360cef70fef45c743a80f2bffcd353151e0230c1c5fc2cced3e729511746121) 
+    set(commitId 54910b9b546225e3382be8a584b05291c01a1531)
+    set(sha512 cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e)
     vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO ankurvdev/stencil
