7df9c1ce57ee7ca95a6920d00dc08e8f746bfccd
ankurv <ankurv@microsoft.com>
2022-08-14 00:07:22 -0700

    Update stencil port

--- ci/vcpkg-additional-ports/stencil/portfile.cmake
+++ ci/vcpkg-additional-ports/stencil/portfile.cmake
@@ -12,7 +12,7 @@ elseif (DEFINED ENV{VCPKG_USE_STENCIL_GIT_REPO} AND NOT "$ENV{VCPKG_USE_STENCIL_
     )
 else()
     # sha512sum can be obtained using
-    # wget -O - -q  https://github.com/ankurverma85/stencil/archive/b6429f8b92947273a5e66d5f10210b960616a89d.tar.gz | sha512sum 
+    # wget -O - -q  https://github.com/ankurdev/stencil/archive/b6429f8b92947273a5e66d5f10210b960616a89d.tar.gz | sha512sum 
     set(commitId 91817114bf4f2a92422e9867364e0b0f3fa032e0)
     set(sha512 b43be4e5ec20d480d48b3c38e88c39a4b3a22cd054c6db96523b69c4b1f95c17a9e27d9a76c1e0a6ad27d7e6ad93071f1da795d5789a370e9f56a616000aa49a)
     vcpkg_from_github(
@@ -23,8 +23,8 @@ else()
         HEAD_REF master)
 endif()
 # Dont use these these are older version
-#vcpkg_find_acquire_program(FLEX)
-#vcpkg_find_acquire_program(BISON)
+vcpkg_find_acquire_program(FLEX)
+vcpkg_find_acquire_program(BISON)
 if(NOT TARGET_TRIPLET STREQUAL HOST_TRIPLET)
     vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools")
 endif()
@@ -35,6 +35,8 @@ vcpkg_configure_cmake(
     OPTIONS
         -DSTENCIL_INSTALL_BUILDTOOLS=ON
         -DBUILD_TESTING=OFF
+        -DBISON_EXECUTABLE=${BISON}
+        -DFLEX_EXECUTABLE=${FLEX}
 )
 
 vcpkg_install_cmake()
