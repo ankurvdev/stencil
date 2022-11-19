85217bbb3d9ff4b3132c228cd37338dee4b61ea1
Ankur Verma <ankur.verma@outlook.com>
2022-11-19 04:36:43 +0000

    Update VCPKG Port to 6c3e505f362fa99ce61aa1319c070f961320b8b8

--- ci/vcpkg-additional-ports/stencil/portfile.cmake
+++ ci/vcpkg-additional-ports/stencil/portfile.cmake
@@ -1,7 +1,7 @@
 # sha512sum can be obtained using
 # wget -O - -q  https://github.com/ankurdev/stencil/archive/b6429f8b92947273a5e66d5f10210b960616a89d.tar.gz | sha512sum
-set(commitId 924b46e2e846c234ca0e5a2112ba417be0230eba)
-set(sha512 3463430c79e70286802486557ae693ce446c6db1def8011bb41b33c545616faf268c1a30512118b0fe2046b876a3243406240a07c4d3aca8f26a8c2719bdf0d8)
+set(commitId 6c3e505f362fa99ce61aa1319c070f961320b8b8)
+set(sha512 5e181f18b3950afbeab6f6addb01914a6f00159769b15595c3917deb2645f092b12c8eee11e992ca6d9b064433765a81b009a7660a7324a3e7764355416cadd2)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ankurvdev/stencil
