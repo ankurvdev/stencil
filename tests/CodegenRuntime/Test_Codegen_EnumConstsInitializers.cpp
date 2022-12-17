383fb946571eb6167870e1233e5315b9665384e3
ankurv <ankurv@microsoft.com>
2022-12-16 22:55:45 -0800

    Added test case

--- tests/CodegenRuntime/Test_Codegen_EnumConstsInitializers.cpp
+++ tests/CodegenRuntime/Test_Codegen_EnumConstsInitializers.cpp
@@ -8,10 +8,16 @@
 #include <functional>
 #include <span>
 
-
 TEST_CASE("EnumConstsInitializers", "[EnumConstsInitializers]")
 {
-    SECTION("Simple") {
-    
+    SECTION("Simple")
+    {
+        EnumConstsInitializers::DefaultValues obj{};
+        CHECK(obj.enum1 == EnumConstsInitializers::EnumType1::unknown);
+        CHECK(obj.enum3 == EnumConstsInitializers::EnumType2::three);
+        CHECK(obj.val1 == "conststring");
+        CHECK(obj.val2 == 100);
+        CHECK(obj.val3 == Catch::Approx(100.0));
+        CHECK(obj.val4 == Catch::Approx(101.1234));
     }
 }
