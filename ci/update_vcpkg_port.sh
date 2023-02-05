47c2762039e1ee288664122297e6d779839da7b9
Ankur <ankur.verma@outlook.com>
2023-02-05 02:14:56 -0800

    Fix CI vcpkg update script

--- ci/update_vcpkg_port.sh
+++ ci/update_vcpkg_port.sh
@@ -2,34 +2,40 @@
 set -e
 set -o pipefail
 
-commitId=$(git rev-parse HEAD)
-shortCommitId=$(git rev-parse --short HEAD)
 scriptdir=$(dirname $(readlink -f $0))
-project=$(git config remote.origin.url | grep -oE 'ankurvdev/.*' | cut - -d '/' -f2)
+commitId=$(git -C ${scriptdir} rev-parse HEAD)
+shortCommitId=$(git -C ${scriptdir} rev-parse --short HEAD)
+project=$(git -C ${scriptdir} config remote.origin.url | grep -oE 'ankurvdev/[^.]*' | cut - -d '/' -f2)
+
+echo "${scriptdir} ${project} ${commitId} ${shortCommitId}"
+
 portfile=${scriptdir}/vcpkg-additional-ports/${project}/portfile.cmake
 portjson=${scriptdir}/vcpkg-additional-ports/${project}/vcpkg.json
 sha512=$(wget -O - -q  https://github.com/ankurvdev/${project}/archive/${commitId}.tar.gz | sha512sum -z | cut - -d' ' -f1)
+echo "SHA512:${sha512}"
 
 sed -ri "s/set\(commitId [a-f0-9]+\)/set(commitId ${commitId})/g" ${portfile}
 sed -ri "s/set\(sha512 [a-f0-9]+\)/set(sha512 ${sha512})/g"       ${portfile}
 awk -F'["]' -v OFS='"'  '/"version":/{split($4,a,".");$4=a[1]"."a[2]"."a[3]+1};1'  ${portjson} > ${portjson}.new
 mv ${portjson}.new ${portjson}
-git diff
-git checkout ${commitId}
-git rev-parse --short HEAD
+git -C ${scriptdir} diff
+git -C ${scriptdir} checkout ${commitId}
+git -C ${scriptdir} rev-parse --short HEAD
 git -c user.email="ankur.verma@outlook.com" -c  user.name="Ankur Verma" -C ${scriptdir} commit ${portfile} ${portjson} -m "Update VCPKG Port to ${commitId}"
-git checkout -b vcpkg.${shortCommitId}
-git push origin vcpkg.${shortCommitId} --set-upstream
-git checkout -b vcpkg
-git push origin vcpkg --force --set-upstream
+git -C ${scriptdir} checkout -b vcpkg.${shortCommitId}
+git -C ${scriptdir} checkout -b vcpkg
 
 # Uppdate vcpkg
-vcpkgurl=$(git config remote.origin.url | grep -oE '.*/ankurvdev')/vcpkg
+vcpkgurl=$(git -C ${scriptdir} config remote.origin.url | grep -oE '.*ankurvdev')/vcpkg
 vcpkgdir=${scriptdir}/vcpkg.tmp
+echo "${vcpkgurl} ${vcpkgdir}"
 git clone ${vcpkgurl} ${vcpkgdir} --depth 1 -b ankurv/${project}
 cp ${portfile} ${vcpkgdir}/ports/${project}/portfile.json
 cp ${portjson} ${vcpkgdir}/ports/${project}/vcpkg.json
 git -C ${vcpkgdir} diff
 git -c user.email="ankur.verma@outlook.com" -c  user.name="Ankur Verma" -C ${vcpkgdir} commit -a -m "Update VCPKG Port to ${commitId}"
 git -C ${vcpkgdir} log
-git push
+
+git -C ${scriptdir} push origin vcpkg.${shortCommitId} --set-upstream
+git -C ${scriptdir} push origin vcpkg --force --set-upstream
+git -C ${vcpkgdir} push
