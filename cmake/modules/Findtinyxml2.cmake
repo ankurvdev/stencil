faed3f0e47771f15c019669bdf03d1818908d9cb
Ankur Verma <ankurv@microsoft.com>
2023-10-25 12:54:37 -0700

    Fix warnings on tests

--- cmake/modules/Findtinyxml2.cmake
+++ cmake/modules/Findtinyxml2.cmake
@@ -11,7 +11,7 @@ FetchContent_Declare(
 if (COMMAND vcpkg_install)
     vcpkg_install(tinyxml2)
 endif()
-
+set(tinyxml2_BUILD_TESTING OFF CACHE BOOL "Turn off tinyxml2 testing")
 FetchContent_MakeAvailable(tinyxml2)
 if (COMMAND SupressWarningForTarget)
     SupressWarningForTarget(tinyxml2)
