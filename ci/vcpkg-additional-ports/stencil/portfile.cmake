373cc53c3c44d308029d8ca072fe8b930549962f
Ankur <ankurv@verma>
2023-10-27 21:24:24 -0700

    Sync files

--- ci/vcpkg-additional-ports/stencil/portfile.cmake
+++ ci/vcpkg-additional-ports/stencil/portfile.cmake
@@ -5,15 +5,10 @@ vcpkg_from_github(
     SHA512 b4d445d7621ae90b31c08a7b3ca6c48311c542f3cf03de8866d29e9aa802a2fdc0dc898340ba8406433600aba801bd5646455423f24ee08d5fcd44a10b6e1e6f
     HEAD_REF main)
 
-vcpkg_find_acquire_program(FLEX)
-vcpkg_find_acquire_program(BISON)
-    
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DBUILD_TESTING=OFF
-        -DBISON_EXECUTABLE=${BISON}
-        -DFLEX_EXECUTABLE=${FLEX}
 )
 
 vcpkg_cmake_install()
@@ -22,8 +17,10 @@ vcpkg_copy_pdbs()
 # Handle copyright
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-if(HOST_TRIPLET STREQUAL TARGET_TRIPLET)
+if(HOST_TRIPLET STREQUAL TARGET_TRIPLET) # Otherwise fails on wasm32-emscripten
     vcpkg_copy_tools(TOOL_NAMES stencil AUTO_CLEAN)
+else()
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin")
 endif()
 
 file(READ "${CURRENT_PACKAGES_DIR}/share/stencil/stencilConfig.cmake" config_contents)
