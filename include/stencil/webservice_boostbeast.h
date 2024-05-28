9575dfc25fe03e75eafa43596c3773f26bd06ae8
ankurv <ankur@verma>
2024-05-28 11:55:50 -0700

    fix hang

--- include/stencil/webservice_boostbeast.h
+++ include/stencil/webservice_boostbeast.h
@@ -103,6 +103,7 @@ struct SSEListenerManager
 
     struct Instance : std::enable_shared_from_this<Instance>
     {
+        using time_point = std::chrono::time_point<std::chrono::system_clock>;
         struct SSEContext
         {
             SSEContext(tcp_stream& streamIn, Request const& reqIn) : stream(streamIn)
@@ -122,6 +123,7 @@ struct SSEListenerManager
         SSEListenerManager*     _manager{nullptr};
         SSEContext*             _ctx{nullptr};
         std::condition_variable _dataAvailable{};
+        time_point              _lastSendAt;
         bool                    _stopRequested = false;
 
         Instance()  = default;
@@ -137,16 +139,19 @@ struct SSEListenerManager
                 _manager->Register(lock, shared_from_this());
                 if (_manager->_stopRequested) return;
                 _ctx = &ctx;
-
-                Send(msg);
+                Send(lock, msg);
             }
             do {
-                auto lock = std::unique_lock<std::mutex>(_manager->_mutex);
+                auto constexpr KeepAliveInterval = 10s;
+                auto lock                        = std::unique_lock<std::mutex>(_manager->_mutex);
                 if (_manager->_stopRequested) return;
-                _dataAvailable.wait_for(lock, 10s, [&](...) { return _streamEnded() || _manager->_stopRequested; });
+                auto status = _dataAvailable.wait_for(lock, KeepAliveInterval);
+                if (status == std::cv_status::timeout && ((_lastSendAt + KeepAliveInterval) < std::chrono::system_clock::now()))
+                {    // timed out
+                    if (!Send(lock, "\n\n")) { return; }
+                }
                 if (_manager->_stopRequested) return;
                 if (_streamEnded()) { return; }
-                Send("\n\n");
             } while (true);
         }
 
@@ -157,13 +162,22 @@ struct SSEListenerManager
             _dataAvailable.notify_all();
         }
 
-        void Send(std::span<const char> const& msg)
+        bool Send(std::unique_lock<std::mutex> const& lock, std::span<const char> const& msg)
         {
-            if (msg.size() == 0) return;
+            if (_streamEnded()) return false;
+            if (msg.size() == 0) return true;
+            _lastSendAt  = std::chrono::system_clock::now();
             auto msgSize = msg[msg.size() - 1] == '\0' ? msg.size() - 1 : msg.size();
-            if (msgSize == 0) return;
+            if (msgSize == 0) return true;
             boost::asio::const_buffer b{msg.data(), msgSize};
-            boost::beast::net::write(_ctx->stream, boost::beast::http::make_chunk(b));
+            boost::system::error_code ec;
+            boost::beast::net::write(_ctx->stream, boost::beast::http::make_chunk(b), ec);
+            if (ec.failed())
+            {
+                Release(lock);
+                return false;
+            }
+            return true;
         }
     };
 
@@ -175,15 +189,11 @@ struct SSEListenerManager
             if (_stopRequested) return;
             auto inst = *it;
             if (inst->_stopRequested) continue;
-            try
+            if (!inst->Send(lock, msg)) { it = _sseListeners.erase(it); }
+            else
             {
-                inst->Send(msg);
                 inst->_dataAvailable.notify_all();
                 ++it;
-            } catch (...)
-            {
-                inst->Release(lock);
-                it = _sseListeners.erase(it);
             }
         }
     }
