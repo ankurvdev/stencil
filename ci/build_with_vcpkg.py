d5d7c9354d4321ccf38b4bba5a3eb189e6bf5fd7
ankurv <ankur@verma>
2024-07-07 17:57:45 -0700

    Fixes

--- ci/build_with_vcpkg.py
+++ ci/build_with_vcpkg.py
@@ -45,7 +45,7 @@ if not vcpkgroot.exists():
             "-q",
             "https://github.com/ankurvdev/vcpkg.git",
             "--branch",
-            "lkg_patched",
+            "origin/master",
             "--depth",
             "1",
         ],
