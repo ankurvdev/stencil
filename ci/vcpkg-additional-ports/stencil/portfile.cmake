9dc5eb1ad22358973db2bf684ea416fa7f549668
Ankur Verma <ankur.verma@outlook.com>
2025-06-22 02:45:57 +0000

    Update VCPKG Port to b7b91b3e81de2a137d390c27658d111dd198e702

--- ci/vcpkg-additional-ports/stencil/portfile.cmake
+++ ci/vcpkg-additional-ports/stencil/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ankurvdev/stencil
-    REF "7986c016559cc70fa8947ac86cd032120c9f0ed5"
-    SHA512 1ded8c1bfcbb9ca5cf35e0c494d288dd508d25f5fa4bdeb6f5126e086f5a16a019829dcdf95ba62239327e7568df6aae11f26b0578ad61b1fc041d6709dfd952
+    REF "b7b91b3e81de2a137d390c27658d111dd198e702"
+    SHA512 f7b544eb157be24123e365e5ffbf78f5b55a6a0e7ee6f4a2110095f1226e2ad99abf1236f27f26aa6b13b7d931b5d0d5571556af70f5713ad71bbaf28f9510eb
     HEAD_REF main)
 
 vcpkg_cmake_configure(
