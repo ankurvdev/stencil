2b65d10d9847da7a1eadf20c20819641edd9be0f
ankurv <ankur@verma>
2025-10-16 02:18:12 -0700

    Fix crash

--- include/stencil/interfaces.h
+++ include/stencil/interfaces.h
@@ -66,9 +66,10 @@ template <typename T> struct InterfaceT
 {
     template <typename TArgStruct> void RaiseEvent(TArgStruct const& args)
     {
+        if (handlerptr == nullptr) { return; }
         auto handlers = reinterpret_cast<impl::Interface::InterfaceEventHandlers<T>*>(handlerptr);
         auto handler  = std::get<impl::Interface::IEventHandler<TArgStruct>*>(handlers->handlers);
-        if (handler) { handler->HandleEvent(args); }
+        handler->HandleEvent(args);
     }
 
     template <typename THandler> void SetHandler(THandler* ptr)
