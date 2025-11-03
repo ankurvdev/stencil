c0c0b542a2b53a9ba7a3cdcfd71d9f29d2741c03
Ankur <ankurv@verma>
2025-11-02 18:16:52 -0800

    try 8

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -6,5 +6,5 @@ set -x
 
 cmake $(dirname $(realpath $0))/..
 # Building with -j causes devops agents to run out of memory
-cmake --build . -j4 --verbose --target package
+cmake --build . -j8 --verbose --target package
 ctest --output-on-failure
